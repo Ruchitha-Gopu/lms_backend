acfe4895b0cb6e2b446265d23b57c8852b4cb8c9
Ruchitha-Gopu <gopuruchitha10@gmail.com>
2026-06-14 16:42:02 +0530

    Fix email sending config

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,18 +3,27 @@ const nodemailer = require("nodemailer");
 const sendEmail = async (to, subject, message) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
 
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
+
+      connectionTimeout: 30000,
+      greetingTimeout: 30000,
+      socketTimeout: 30000,
     });
 
+    await transporter.verify();
+    console.log("SMTP Ready");
+
     await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to,
-      subject,
+      from: `"LMS App" <${process.env.EMAIL_USER}>`,
+      to: to,
+      subject: subject,
       html: message,
     });
 
