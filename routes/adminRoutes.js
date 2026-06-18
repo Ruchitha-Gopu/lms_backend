7acfb4d71dbd041f8314ad2d301b21a153e196f1
Ruchitha-Gopu <gopuruchitha10@gmail.com>
2026-06-18 13:32:52 +0530

    admindashbord_issuse

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-const Student = require("../models/Student");
+const User = require("../models/User");
 const Course = require("../models/Course");
 const Assignment = require("../models/Assignment");
 const Quiz = require("../models/Quiz");
 
 router.get("/reports", async (req, res) => {
   try {
-    const students = await Student.countDocuments();
+    const users = await User.countDocuments();
     const courses = await Course.countDocuments();
     const assignments = await Assignment.countDocuments();
     const quizzes = await Quiz.countDocuments();
 
     res.json({
-      students,
+      users,
       courses,
       assignments,
       quizzes,
